1cb4201b88356f9fd35b2ba258e75648566a511d
Kevin Haro <kevinharo019@gmail.com>
2023-11-27 23:32:22 -0700

    Add files via upload

--- WebPd-main/configs/runtime.rollup.config.js
+++ WebPd-main/configs/runtime.rollup.config.js
@@ -2,30 +2,23 @@ import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { string } from 'rollup-plugin-string'
 import commonjs from '@rollup/plugin-commonjs'
 
+// rollup.config.js
 export default {
-    input: 'main.cli.ts',
+    input: 'main.runtime.ts',
     output: {
-        file: 'bin/cli.mjs',
-        format: 'es',
-        banner: '#!/usr/bin/env node',
+        file: 'src/app-generator/runtime.generated.js.txt',
+        format: 'iife',
+        name: 'WebPdRuntime',
     },
     plugins: [
-        string({
-            include: ['./src/app-generator/runtime.generated.js.txt'],
-        }),
-        typescript({
-            compilerOptions: {
-                declaration: false,
-            },
-        }),
+        typescript({ tsconfig: './configs/runtime.tsconfig.json' }),
         nodeResolve(),
         commonjs(),
         visualizer({
             template: 'network',
-            filename: './tmp/bin-rollup-stats.html',
+            filename: './tmp/runtime-rollup-stats.html',
         }),
         json(),
     ],
